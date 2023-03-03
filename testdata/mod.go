64fb7fba346dacb43d02e7a22d96e7a6860e8a7f
shynome <shynome@gmail.com>
2023-03-03 12:32:23 +0800

    default is right module

--- testdata/mod.go
+++ testdata/mod.go
@@ -1,5 +1,10 @@
 package hello
 
-func Export() {
+import "github.com/wage-run/wage"
 
-}
+type M struct{}
+
+func (m M) Open(input any) error { return nil }
+func (m M) Close() error         { return nil }
+
+func Export() wage.Module { return M{} }
